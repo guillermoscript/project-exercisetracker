ad07210cebe406813e28d3f64255dba05319935a
Guillermo <guillermo.marin@roraimadevs.com>
2023-01-22 19:46:21 -0400

    add models

---
+++ models/models.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const { types } = require('util')
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+});
+
+const exerciseSchema = mongoose.Schema({
+    description: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        validate: (value) => {
+            return types.isDate(value)
+        }
+    }
+})
+
+const user = mongoose.model('User', userSchema);
+const exercise = mongoose.model('Exercise',exerciseSchema);
+
+
+module.exports = {
+    user,
+    exercise
+}

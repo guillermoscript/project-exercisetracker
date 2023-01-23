07d0cfbf0b3025b5158ff595c3bf4c263a8ddbfd
Guillermo <guillermo.marin@roraimadevs.com>
2023-01-22 20:42:53 -0400

    removed coded

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require('cors')
 require('dotenv').config()
 const mongoose = require('mongoose')
 const { getUserLogs, getUsers, createExercise, createUser } = require('./services/index')
-
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
